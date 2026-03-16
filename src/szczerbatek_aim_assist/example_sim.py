94ad43e5ae054441d418b15c0c2538a3e0c1b824
Bartek <krikbart@gmail.com>
2026-03-16 18:31:32 +0100

    matplotlib 3D trajectory

--- src/szczerbatek_aim_assist/example_sim.py
+++ src/szczerbatek_aim_assist/example_sim.py
@@ -1,5 +1,6 @@
 import numpy as np
 import szczerbatek_aim_assist.core_math as rk
+import matplotlib.pyplot as plt
 
 # input
 mass = 2.0
@@ -16,3 +17,16 @@ times, states = rk.simulate_drop(initial_state, mass, cd, area)
 
 print(states[-1])
 print(times[-1])
+
+# plotting 3D trajectory
+x_vals = [state[0] for state in states]
+y_vals = [state[1] for state in states]
+z_vals = [state[2] for state in states]
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+ax.plot(x_vals, y_vals, z_vals)
+ax.set_xlabel("X Position (m)")
+ax.set_ylabel("Y Position (m)")
+ax.set_zlabel("Z Position (m)")
+ax.set_title("3D Trajectory of Dropped Object")
+plt.show()
