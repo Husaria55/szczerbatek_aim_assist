d7cb37b3f2e748b6d432abb5f9381086c6740535
Bartek <krikbart@gmail.com>
2026-03-20 11:40:26 +0100

    example solver with visualization

--- src/szczerbatek_aim_assist/example_sim.py
+++ src/szczerbatek_aim_assist/example_sim.py
@@ -10,10 +10,10 @@ z = 50
 v_x = 25
 x = 0
 y = 0
-
+env = rk.SimulationEnvironment(wind_vector=np.array([10.0, 20.0, 0.0]))
 initial_state = np.array([0.0, 0.0, 50.0, 25.0, 0.0, 0.0])
 
-times, states = rk.simulate_drop(initial_state, mass, cd, area)
+times, states = rk.simulate_drop(initial_state, mass, cd, area, env=env)
 
 print(states[-1])
 print(times[-1])
@@ -29,4 +29,27 @@ ax.set_xlabel("X Position (m)")
 ax.set_ylabel("Y Position (m)")
 ax.set_zlabel("Z Position (m)")
 ax.set_title("3D Trajectory of Dropped Object")
+plt.show()
+
+
+# example finding release point for a target at (100, 0, 0)
+# wirh non zero wind
+
+target_position = np.array([100.0, 0.0, 0.0])
+solver = rk.DropSolver(mass, cd, area)
+approach_altitude = 50.0
+velocity_vector = np.array([25.0, 0.0, 0.0])
+release_point = solver.calculate_release_point(
+    target_position, approach_altitude, velocity_vector, env
+)
+print(f"Release point for target at {target_position}: {release_point}")
+# wisualkization of release point and target
+plt.figure()
+plt.plot(target_position[0], target_position[1], "ro", label="Target")
+plt.plot(release_point[0], release_point[1], "go", label="Release Point")
+plt.xlabel("X Position (m)")
+plt.ylabel("Y Position (m)")
+plt.title("Release Point and Target Location")
+plt.legend()
+plt.grid()
 plt.show()
